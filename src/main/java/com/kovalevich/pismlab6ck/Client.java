8e0b51d7fe3f427b7ee63d58e0cdfa819a7528fc
Gleb Remniov <remniov.work@gmail.com>
2019-05-09 10:34:42 +0300

    Refactoring

--- src/main/java/com/kovalevich/pismlab6ck/Client.java
+++ src/main/java/com/kovalevich/pismlab6ck/Client.java
@@ -6,8 +6,8 @@ import javax.naming.NamingException;
 public class Client {
     public static void main(String[] args) throws NamingException {
 
-        InitialContext ctx = new InitialContext();
-        MyBeanRemote bean = (MyBeanRemote) ctx.lookup(Config.MAPPED_NAME);
+        InitialContext context = new InitialContext();
+        MyBeanRemote bean = (MyBeanRemote) context.lookup(Config.MAPPED_NAME);
 
         System.out.println(bean.getWorkers());
     }
